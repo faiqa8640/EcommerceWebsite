e9960b19b049a874d527579e6ce0794d730dc849
faiqa20 <faiqamustafa02@gmail.com>
2026-06-22 12:30:40 +0500

    Update CORS for production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true })); // covert the html data into th
 // );
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+    origin: [process.env.CLIENT_URL || "http://localhost:5173"],
     credentials: true,
   })
 );
